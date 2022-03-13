f900ee06f3bbe32e8f90e2f9579e9dbf62ee08aa
Aleja Ocampo <omaira.527@hotmail.com>
2022-03-12 20:16:11 -0500

    primer commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-dark">
+      <h1>Navbar</h1>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ src/components/PokemonList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Pokemon from "./Pokemon";
+
+const PokemonList = () => {
+  return (
+    <div>
+      <Pokemon />
+    </div>
+  );
+};
+
+export default PokemonList;
